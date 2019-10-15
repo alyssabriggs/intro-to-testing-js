cec8d0dcb1cd4bf83371eedacb7d92bef64f412d
alyssalanae <briggs.l.alyssa@gmail.com>
2019-10-15 09:56:53 -0500

    Completed exercise 8

--- code.js
+++ code.js
@@ -2,11 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
 function sayHello(input) {
-    if (input === true){
+    if(typeof input == 'undefined') {
+        return "Hello, World!";
+    } if (input === true){
         return "Hello, World!";
     } else if (input === false) {
         return "Hello, World!";
+    } else {
+        return "Hello, " + input + "!";
     }
-    return "Hello, " + input + "!";
 }
